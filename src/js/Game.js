54d8b484420254b4c8ad4e4e8a61950e43bf27fc
TorchAblaze <tiffanygreathead@gmail.com>
2021-04-14 15:51:31 -0700

    Add passing startBattle test
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Jeremy Banka <jeremybanka@gmail.com>

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,15 +1,30 @@
+const EXPLORE = "explore";
+const BATTLE = "battle";
+const LOOT = "loot";
+const WASTED = "wasted";
+export const STATE = {
+  EXPLORE,
+  BATTLE,
+  LOOT,
+  WASTED,
+};
+
 export default class Game {
   constructor() {
     this.idTicker = -1;
     this.turnOrder = [];
     this.characters = {};
-    this.state = "";
-    this.otherStates = ["explore", "battle", "loot", "wasted"];
+    this.state = EXPLORE;
+    this.otherStates = [BATTLE, LOOT, WASTED];
   }
   addCharacter(character) {
     this.idTicker += 1;
     const id = this.idTicker;
     character.id = id;
     this.characters[id] = character;
+  }
+  startBattle() {
+    this.otherStates.push(this.state);
+    this.state = this.otherStates.shift();
   }
 }
