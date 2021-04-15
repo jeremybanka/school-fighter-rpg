7034a773c57bb38cbba9050da232121862e36d95
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-15 15:01:15 -0700

    add type.js file to characters directory Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com> Co-authored-by: Nick Reeder <nickreeder32@gmail.com> co-authored-by: Min Chang <minchangmhc@gmail.com>

---
+++ src/js/characters/type.js
@@ -0,0 +1,15 @@
+export default class Type {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+}
+
+// let bully = new Bully()
+// bully.addType() {
+// let type = new Type("super strong", "Although the bully is strong, he's also dumb and slow.");
+// this.type = type;
+//}
+
+// What can the super strong type do? character.type.description = "Although the bully is strong, he's also dumb and slow."
+// player came upon a rock. if character.type.name === "super strong", then player can pick it up.
